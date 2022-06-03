23419ac25ae8b7471bfc7268d3c8129a8bc1a752
David Stockdale <dstock3@gmail.com>
2022-06-03 18:49:24 -0500

    Added api call in Main component to be fleshed out when backend is ready.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,32 @@
+import React, { useEffect } from 'react'
+
 const Main = () => {
+    /* 
+    useEffect(()=> {
+        let apiCall = ``
+    
+        fetch(apiCall)
+          .then(
+              function(response) {
+                  return response.json()
+              }
+          )
+          .then(
+              function(data) {
+
+    
+    
+              }
+          )
+          .catch(
+              function(err) {
+                  console.log(err)
+              }
+          )
+      
+      }, []) 
+      */
+
     return (
         <main className="blog">
             <div className="sidebar"></div>
