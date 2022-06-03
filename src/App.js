0b9b20d84ab276c99f934be0d57e40a9abe470bd
David Stockdale <dstock3@gmail.com>
2022-06-03 17:48:21 -0500

    Added basic components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      test
-
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
