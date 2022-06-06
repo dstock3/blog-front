acf3cd866ee772cac3516c519502a79deb23be0b
David Stockdale <dstock3@gmail.com>
2022-06-05 22:40:03 -0500

    Added toggle for settings display/

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,20 @@
+import React, { useEffect } from 'react'
+
 const Settings = () => {
+    useEffect(()=> {
+        const settings = document.getElementsByClassName("settings")[0];
+        settings.addEventListener("click", function() {
+            if (settings.classList.contains("hidden")) {
+                settings.classList.remove("hidden")
+                settings.classList.add("visible")
+            } else {
+                settings.classList.remove("visible")
+                settings.classList.add("hidden")
+            }
+        })
+
+    }, [])
+
     return (
         <div className="settings hidden">Settings</div>
     );
