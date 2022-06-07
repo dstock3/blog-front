40a0f8d1dca0dd801fcf8385d14f7cca3ed4eeb4
David Stockdale <dstock3@gmail.com>
2022-06-07 16:49:43 -0500

    Revised Main component to render a maximum of 3 articles.

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,12 @@ const Main = ({articles, userInfo}) => {
     return (
         <main className="blog">
             <Sidebar userInfo={userInfo} articles={articles}/>
-            <Article article={articles["0"]}/>
+            <div className="articles-container">
+                {Object.values(articles).map((article, index) => {
+                    return index < 3 ?
+                        <Article key={index} article={articles[index]}/> : null;
+                })}
+            </div>
         </main>
     );
 }
