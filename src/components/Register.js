290c4e8b887072bfab70f77ab3d5d229bc92d06d
David Stockdale <dstock3@gmail.com>
2022-06-09 16:45:20 -0500

    Fleshed out Register component.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,23 @@
 const Register = () => {
     return (
         <div className="register">
+            <form className="registerForm" action="" method="POST">
+                
+                <div className="user-register-container">
+                    <label for="username">Username: </label>
+                    <input type="text" name="username"></input>
+                </div>
+
+                <div className="user-register-container">
+                    <label for="password">Password: </label>
+                    <input type="password" name="password"></input>
+
+                    <label for="confirmPassword">Confirm Password: </label>
+                    <input type="password" name="confirmPassword"></input>
+                </div>
+                
+                <button className="submit-btn">Login</button>
+            </form>
 
         </div>
     );
