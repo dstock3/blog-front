5ee29d349850501b7724373b09c74e38914836c6
David Stockdale <dstock3@gmail.com>
2022-06-10 17:10:53 -0500

    Fleshed out the Compose component.

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -7,9 +7,21 @@ const Compose = ({userInfo, articles, theme}) => {
             <main className={"compose-page" + theme.main}>
                 <Sidebar userInfo={userInfo} articles={articles} theme={theme} />
 
-                
-    
-                
+                <form action="" method="POST">
+                    <div className="compose-title-container">
+                        <label for="title">Title:</label>
+                        <input type="text" for="title"></input>
+                    </div>
+
+                    <div className="compose-body-container">
+                        <label for="content">Content:</label>
+                        <textarea type="text" for="content"></textarea>
+                    </div>
+
+                    <div className="compose-options">
+                        <button className="submit-btn">Submit</button>
+                    </div>
+                </form>
             </main>
         );
     } else {
