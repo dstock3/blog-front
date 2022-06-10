0f19180622bf43ef0bcaf7bbf9a7d89e460dcaff
David Stockdale <dstock3@gmail.com>
2022-06-10 17:15:01 -0500

    Added details to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
 const Footer = ({theme}) => {
     return (
         <footer className={theme.main}>
-
-            
+            <div className="attribution">
+                BlogDog CMS was created by David Stockdale.
+            </div>
+            <div className="social-links">
+                <a href="https://github.com/dstock3" target="_blank" rel="noopener noreferrer">Github</a>
+                <a href="https://davestockdale.io/" target="_blank" rel="noopener noreferrer">Portfolio</a>
+            </div>
         </footer>
     );
 }
