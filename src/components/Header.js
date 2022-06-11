b316bad26b5391effbf6b20a4ac276c0007fefcd
David Stockdale <dstock3@gmail.com>
2022-06-11 17:35:28 -0500

    Added link to h1 title element in Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import Settings from "./Settings";
 
 const Header = ({theme, title}) => {
     return (
         <header className={theme.main}>
-            <h1 className="title">{title}</h1>
+                <h1 className="title">
+                    <Link to = {{pathname: '/'}}>
+                        {title}
+                    </Link>
+                </h1>
+
             <Settings theme={theme} /> 
         </header>
     );
