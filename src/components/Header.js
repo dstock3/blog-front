89ecbc51ca2411ac7b75311ed3b08120bb872982
David Stockdale <dstock3@gmail.com>
2022-06-12 18:45:26 -0500

    Added useEffect call in Header component in order to determine if a theme has been set. If not, it uses light as the default theme.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Settings from "./Settings";
 
 const Header = ({theme, title}) => {
+    const [thisTheme, setThisTheme] = useState("light")
+
+    useEffect(()=> {
+        if (theme) {
+            setThisTheme(theme.main)
+        }
+    }, [])
+    
     return (
-        <header className={theme.main}>
+        <header className={thisTheme}>
                 <h1 className="title">
                     {title ?
                         <Link to = {{pathname: '/'}}>{title}</Link> :
