77f37803ee54118852404690c262bac517189755
David Stockdale <dstock3@gmail.com>
2022-06-13 16:05:08 -0500

    Added switch statement in Article component to check for image info.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,16 @@
-const Article = ({article, theme, layout, option}) => {
-
-
+const Article = ({article, theme, layout}) => {
     return (
         <article className={theme.main + " " + layout.child}>
             <div className="article-head">
                 <h1 className="article-name">{article["title"]}</h1>
                 <div className="date-posted">{article["date"]}</div>
             </div>
-            
+            {article["img"] ?
+                <div className="img-container">
+                    <img className="article-img" src={article["img"]} alt={article["img-desc"]}></img>
+                </div> :
+                null
+            }
             <div className="article-content">{article["content"]}</div>
         </article>
     );
