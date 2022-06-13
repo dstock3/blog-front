3768d02a80e91bf5552853b64a5d3de69a6f24b5
David Stockdale <dstock3@gmail.com>
2022-06-12 20:50:48 -0500

    Added css specs for card view.

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -16,6 +16,7 @@ const Settings = ({theme}) => {
         <>
             <div onClick={toggleStatus} className="settings-icon">Settings</div>
             <div className={"settings-dropdown " + optionsStatus + " " + theme.main}>
+                {/* Thus far, settings should include theme, view (card or basic), edit profile, and logout */}
                 <ul className="options-list">
                     <li className="option-item">Option 1</li>
                     <li className="option-item">Option 2</li>
