cee7df40ab87ca9f5c697b8d169844739544e10f
David Stockdale <dstock3@gmail.com>
2022-06-14 13:13:52 -0500

    Created sidebar stylesheet.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import Profile from './Profile'
 import Archive from './Archive'
+import '../style/sidebar.css'
 import { Link } from 'react-router-dom';
 
 const Sidebar = ({articles, userInfo, theme}) => {
