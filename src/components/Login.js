52e2bfb3b967c0681647d08d6b241ee4680511fb
David Stockdale <dstock3@gmail.com>
2022-06-22 15:14:54 -0500

    Added attributes to login form. Added register prompt as well.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,27 @@
+import { Link } from 'react-router-dom';
 import '../style/login.css'
 
 const Login = () => {
     return (
         <div className="login">
             <form className="loginForm" action="" method="POST">
-                <h2>Login</h2>
+                <h2 className="form-head">Login</h2>
                 <div className="user-login-container">
-                    <label for="username">Username: </label>
+                    <label className="log-label" for="username">Username: </label>
                     <input type="text" name="username"></input>
                 </div>
 
                 <div className="user-password-container">
-                    <label for="password">Password: </label>
+                    <label className="log-label" for="password">Password: </label>
                     <input type="password" name="password"></input>
                 </div>
 
-                <button className="submit-btn">Login</button>
+                <button className="submit-btn log-btn">Login</button>
             </form>
+            <div className="prompt-container">
+                <div className="reg-prompt">Don't have an Account?</div>
+                <Link to = {{pathname: '/register'}}>Register</Link>
+            </div>
         </div>
     );
 }
