4bfc183614ab98e0e308d097bab2cf9d836f7f8b
David Stockdale <dstock3@gmail.com>
2022-07-06 15:28:54 -0500

    fleshed out Compose component.

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -29,6 +29,7 @@ const Compose = ({userInfo, articles, theme}) => {
             let resJson = await res.json();
 
             if (res.status === 200) {
+                console.log(res.body)
                 setTitle("")
                 setImg("")
                 setImgDesc("")
@@ -45,24 +46,41 @@ const Compose = ({userInfo, articles, theme}) => {
 
     if (userInfo) {
         return (
-            <main className={"compose-page " }>
+            <main className="compose-page">
                 <Sidebar userInfo={userInfo} articles={articles} theme={theme} />
 
-                <form className={"composeForm " + theme.main} action="" method="POST">
+                <form onSubmit={handleSubmit} className={"composeForm " + theme.main} action="" method="POST">
+                    <div className="message">{message ? <p>{message}</p> : null}</div>
                     <div className="compose-subcontainer compose-title">
-                        <label className="compose-label" for="title">Title:</label>
-                        <input className="compose-title-input" type="text" for="title"></input>
+                        <label className="compose-label" htmlFor="title">Title:</label>
+                        <input className="compose-title-input" value={title} type="text" htmlFor="title" name="title" onChange={(e) => setTitle(e.target.value)}></input>
+                    </div>
+
+                    <div className="compose-subcontainer compose-date">
+                        <label className="compose-label" htmlFor="date">Date:</label>
+                        <input className="compose-title-input" value={date} type="text" htmlFor="date" name="date" onChange={(e) => setDate(e.target.value)}></input>
+                    </div>
+
+                    <div className="compose-subcontainer compose-img">
+                        <label className="compose-label" htmlFor="img">Image:</label>
+                        <input className="compose-title-input" value={img} type="text" htmlFor="img" name="img" onChange={(e) => setImg(e.target.value)}></input>
+                    </div>
+
+                    <div className="compose-subcontainer compose-img-desc">
+                        <label className="compose-label" htmlFor="imgDesc">Image Caption:</label>
+                        <input className="compose-title-input" value={imgDesc} type="text" htmlFor="imgDesc" name="imgDesc" onChange={(e) => setImgDesc(e.target.value)}></input>
                     </div>
 
                     <div className="compose-subcontainer compose-body">
-                        <label className="compose-label"for="content">Content:</label>
-                        <textarea className="compose-content-input" type="text" for="content" rows="25"></textarea>
+                        <label className="compose-label" htmlFor="content">Content:</label>
+                        <textarea className="compose-content-input" type="text" value={content} name="content" htmlFor="content" rows="25" onChange={(e) => setContent(e.target.value)}></textarea>
                     </div>
 
                     <div className="compose-subcontainer compose-options">
-                        <button className="submit-btn">Submit</button>
+                        <button type="submit" className="submit-btn">Submit</button>
                     </div>
                 </form>
+                
             </main>
         );
     } else {
