3380d421eef987f6725624424e5d8cf01bba311e
David Stockdale <dstock3@gmail.com>
2022-07-07 01:00:47 -0500

    Added hamburger menu icon. Repositioned dropdown menu.:

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import menuIcon from '../images/hamburger_menu.svg'
 
 const Settings = ({theme}) => {
     const [optionsStatus, setOptionsStatus] = useState("hidden")
@@ -19,18 +20,20 @@ const Settings = ({theme}) => {
 
     return (
         <>
-            <div onClick={toggleStatus} className="settings-icon">Settings</div>
+            <div onClick={toggleStatus} className="settings-icon">
+                <img src={menuIcon} alt="menu icon"></img>
+            </div>
             <div className={"settings-dropdown " + optionsStatus + " " + theme.main}>
                 {/* Thus far, settings should include theme, view (card or basic), edit profile, and logout */}
                 <ul className="options-list">
+                    <li className="option-item">
+                        <Link to ="/" onClick={() => setOptionsStatus("hidden")}>Home</Link>
+                    </li>
                     <li className="option-item">
                         <Link to ="/login" onClick={() => setOptionsStatus("hidden")}>Sign In</Link>
                     </li>
                     <li className="option-item">
                         <Link to ="/register" onClick={() => setOptionsStatus("hidden")}>Register</Link>
-                    </li>
-                    <li className="option-item">
-                        <Link to ="/" onClick={() => setOptionsStatus("hidden")}>Home</Link>
                     </li>
                     <li className="option-item">
                         <Link to ="/options" onClick={() => setOptionsStatus("hidden")}>Options</Link>
