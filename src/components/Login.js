fe049d1c563d95f4414f0113d244972a01bc84fe
David Stockdale <dstock3@gmail.com>
2022-07-07 18:51:17 -0500

    Started working on loginHandler function in Login component.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/login.css'
 
 const Login = () => {
+    const [username, setUsername] = useState("")
+    const [pw, setPw] = useState("")
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+        try { 
+
+        } catch { 
+
+        }
+
+    }
+
     return (
         <div className={"register dark"}>
-            <form className="loginForm" action="" method="POST">
+            <form onSubmit={loginHandler} className="loginForm" action="" method="POST">
                 <h2 className="form-head">Login</h2>
                 <div className="user-login-container">
                     <label className="log-label" htmlFor="username">Username: </label>
-                    <input type="text" name="username"></input>
+                    <input value={username} type="text" name="username" onChange={(e) => setUsername(e.target.value)}></input>
                 </div>
 
                 <div className="user-password-container">
                     <label className="log-label" htmlFor="password">Password: </label>
-                    <input type="password" name="password"></input>
+                    <input value={pw} type="password" name="password" onChange={(e) => setPw(e.target.value)}></input>
                 </div>
 
                 <button className="submit-btn log-btn">Login</button>
