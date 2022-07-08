89c3396fe96fd0ca19031e854d8e3500bd58f280
David Stockdale <dstock3@gmail.com>
2022-07-08 14:19:45 -0500

    Added api call for logout page.

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Logout = () => {
-  return (
-    <div>Logout</div>
-  )
+    const [logoutMessage, setLogoutMessage] = useState("")
+    useEffect(()=> {
+        let apiCall = 'https://stormy-waters-34046.herokuapp.com/logout'
+        
+        fetch(apiCall)
+        .then(
+            function(response) {
+                return response.json()
+            }
+        )
+        .then(
+            function(data) {
+                if (data["message"] === "logout successful") {
+                    setLogoutMessage("You have successfully logged out.")
+                } else {
+                    setLogoutMessage("There was a problem with your request.")
+                }
+            }
+        )
+        .catch(
+            function(err) {
+                console.log(err)
+            }
+        )
+    }, [])
+
+    return (
+        <main className="logout-page">
+            <div className="logout-message">{logoutMessage}</div>
+        </main>
+        
+    )
 }
 
 export default Logout
