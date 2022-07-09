738780b21d620059bc5dbfcfde8cb6e22dd2182c
David Stockdale <dstock3@gmail.com>
2022-07-09 15:13:18 -0500

    Started working on commentHandler function to make api POST request.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import Prompt from "./Prompt";
 
 const CommentForm = ({userInfo, theme}) => {
+    const [comment, setComment] = useState("")
+
+    const commentHandler = async(e) => {
+        e.preventDefault();
+        try {
+
+        } catch(err) {
+
+        }
+
+    }
+    
     if (userInfo) {
         return (
-            <form className={"comment-form " + theme + "-accent"} action="" method="POST">
+            <form onSubmit={commentHandler} className={"comment-form " + theme + "-accent"} action="" method="POST">
                 <div className="comment-subcontainer comment-prompt">
                     <label className="comment-label" htmlFor="comment">Leave a Comment Below:</label>
-                    <textarea className="comment-input" type="text" htmlFor="comment"></textarea>
+                    <textarea className="comment-input" type="text" value={comment} htmlFor="comment" onChange={(e) => setComment(e.target.value)}></textarea>
                 </div>
                 <div className="comment-btn-subcontainer">
                     <button className="comment-btn">Submit</button>
