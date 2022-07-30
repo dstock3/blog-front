91bb5ca08a759fbaa47e6fd83f8080f8a54631e6
David Stockdale <dstock3@gmail.com>
2022-07-30 13:05:25 -0500

    Added github and code icons to Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+import codeIcon from '../images/code.svg'
+import githubIcon from '../images/github.png'
+
 const Footer = ({theme}) => {
     return (
         <footer className={theme}>
@@ -5,8 +8,12 @@ const Footer = ({theme}) => {
                 BlogDog CMS was created by David Stockdale.
             </div>
             <div className="social-links">
-                <a href="https://github.com/dstock3" target="_blank" rel="noopener noreferrer">Github</a>
-                <a href="https://davestockdale.io/" target="_blank" rel="noopener noreferrer">Portfolio</a>
+                <a className="social-link" href="https://github.com/dstock3" target="_blank" rel="noopener noreferrer">
+                    <img className="github-icon" src={githubIcon} alt="github icon"></img>
+                </a>
+                <a className="social-link" href="https://davestockdale.io/" target="_blank" rel="noopener noreferrer">
+                    <img className="code-icon" src={codeIcon} alt="code icon"></img>
+                </a>
             </div>
         </footer>
     );
